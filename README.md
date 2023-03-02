d429014c894b4bc8957a29d00c79f3e76e4da5a4
birdwatcherYT <aabcehmu.362880@hotmail.co.jp>
2023-03-03 00:22:32 +0900

    チュートリアルのボタン作成

--- README.md
+++ README.md
@@ -28,12 +28,26 @@
 		- `git config --global core.editor emacs`
 	- `#Please enter the commit message for your changes. ...`
 		- コミットメーセージを一番上に書いて保存して終了
-- settings → Pages → Branch
+- ホスティング: settings → Pages → Branch
 	- main/docs
 
 
 ## typescript
 - [サバイバルTypeScript](https://typescriptbook.jp/)
+- npm install -g typescript
+	- tsc -v
+- tsのコンパイル: tsc hogehoge.ts
+- jsの実行: node hogehoge.js
+- yarnのインストール: npm install -g yarn
+	- パッケージ管理ツール
+- yarn start
+	- npmもパッケージマネージャーだがyarnのほうが優れているらしい
+- 拡張子
+	- .ts: 普通のtypescript
+	- .tsx: 中にXMLタグが書ける
+- `import`の`{ xxx }`: [export か export default かの違いらしい](https://blog.bgbgbg.net/archives/4356)
+	- `{ xxx as yyy }`で任意の名前に変更できる
 
 ## React
 - [チュートリアル](https://ja.reactjs.org/tutorial/tutorial.html)
+- `const [値, set関数] = useState(初期値);`
--- app-dir-ts/src/App.tsx
+++ app-dir-ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        テスト
+        <MakeButton />
       </header>
     </div>
   );
+}
+
+function MakeButton() {
+  const [count, setCount] = useState(0);// const [値, set関数] = useState(初期値);
+  const click = () => {
+    setCount(count + 1);
+  };
+  return <span className="myButton" onClick={click}>いいねボタン{count}</span>;
+  // return <div className="myButton" onClick={click}>いいねボタン{count}</div>;
 }
 
 export default App;
